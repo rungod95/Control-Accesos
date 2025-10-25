c1041ec87013f15b795c19f124f27bb0b4ce00ec
javier <jabierplanos@gmail.com>
2025-10-25 19:25:02 +0200

    chore: update security config

--- src/main/java/com/mina/accesos/security/SecurityConfig.java
+++ src/main/java/com/mina/accesos/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.mina.accesos.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
