48cd5db92e86e5000dae7b8d2970ca2032319742
javier <jabierplanos@gmail.com>
2025-11-09 20:05:03 +0100

    fix: enable cors on auth controller

--- src/main/java/com/mina/accesos/controller/AuthController.java
+++ src/main/java/com/mina/accesos/controller/AuthController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     private final AuthenticationManager authManager;
